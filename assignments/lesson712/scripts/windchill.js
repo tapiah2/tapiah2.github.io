9e84e0b491422f949b59b38b270eb2ed6718dd5a
Israel Tapia <35181944+tapiah2@users.noreply.github.com>
2018-02-25 16:22:33 -0600

    lesson 712

---
+++ assignments/lesson712/scripts/windchill.js
@@ -0,0 +1,10 @@
+Add a windchill.js file
+
+to your script folder on the weather site that uses the wind speed and the average temperature between the high and the low predictions to compute and display the wind chill temperature as a new weather data item on to display with the rest of the data.  
+
+The formula to calculate the wind chill factor is: 
+f = 35.74 + 0.6215 t - 35.75 s0.16 + 0.4275 t s0.16  
+where f is the wind chill factor in Fahrenheit, t is the air average temperature in Fahrenheit, and s is the wind speed in miles per hour. 
+Hint: See the Pen JS Basic I/O  
+
+var f()=
